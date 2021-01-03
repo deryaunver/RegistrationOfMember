32bac2234d2acef62a37d626b0007b11749cf075
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-01-03 23:51:17 +0300

    ValidationModule İmplementasyonu

---
+++ RegistrationOfMember.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Ninject.Modules;
+using RegistrationOfMember.Business.ValidationRules.FluentValidation;
+using RegistrationOfMember.Entities.Concrete;
+
+namespace RegistrationOfMember.Business.DependencyResolvers.Ninject
+{
+  public  class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Member>>().To<MemberValidator>().InSingletonScope();
+        }
+    }
+}
