d3cc2d423f1ac53a3e653f6c8787cb78eef07cc2
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-01-03 23:45:34 +0300

    Business Module İmplementasyonu

---
+++ RegistrationOfMember.Business/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+using RegistrationOfMember.Business.Abstract;
+using RegistrationOfMember.Business.Concrete;
+using RegistrationOfMember.Business.ServiceAdapters.Abstract;
+using RegistrationOfMember.Business.ServiceAdapters.Concrete;
+using RegistrationOfMember.DataAccess.Abstract;
+using RegistrationOfMember.DataAccess.Concrete.EntityFramework;
+
+namespace RegistrationOfMember.Business.DependencyResolvers.Ninject
+{
+  public class BusinessModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMemberService>().To<MemberManager>().InSingletonScope();
+            Bind<IMemberDal>().To<EfMemberDal>().InSingletonScope();
+
+            Bind<IKpsService>().To<KpsServiceAdapter>().InSingletonScope();
+        }
+    }
+}
