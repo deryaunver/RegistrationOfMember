118601c1d05b88b5b83aa7cb66f50ab1339082c8
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-01-03 21:51:06 +0300

    İş Katmanı İmplementasyonunun Yapılması

---
+++ RegistrationOfMember.Business/Concrete/MemberManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegistrationOfMember.Business.Abstract;
+using RegistrationOfMember.DataAccess.Abstract;
+using RegistrationOfMember.Entities.Concrete;
+
+namespace RegistrationOfMember.Business.Concrete
+{
+   public class MemberManager:IMemberService
+   {
+       private IMemberDal _memberDal;
+
+       public MemberManager(IMemberDal memberDal)
+       {
+           _memberDal = memberDal;
+       }
+
+       public void Add(Member member)
+       {
+           _memberDal.Add(member);
+       }
+    }
+}
