db5dfcf3d2f99958f24cb270e42feefc90000c7d
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-01-03 19:10:08 +0300

    Entities Katmanı İmplementasyonu

---
+++ RegistrationOfMember.Entities/Concrete/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Entities;
+
+namespace RegistrationOfMember.Entities.Concrete
+{
+    class Member:IEntity
+    {
+        public int Id { get; set; }
+        public  string FirstName  { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public  string Tc { get; set; }
+        public string Email { get; set; }
+
+    }
+}
